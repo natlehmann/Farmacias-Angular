8ebe93adee90e5f686092bc2580981ba9d3d21f8
natlehmann <natlehmann@gmail.com>
2016-06-30 20:48:32 -0300

    configuracion inicial del proyecto

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name farmaciasBaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the farmaciasBaApp
+ */
+angular.module('farmaciasBaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name farmaciasBaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the farmaciasBaApp
+ */
+angular.module('farmaciasBaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
